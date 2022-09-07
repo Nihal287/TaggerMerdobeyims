bae8f2ffae9323405e683c3557bcb7a47cdb3122
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-09-07 18:25:55 +0300

    Update gece.py

--- gece.py
+++ gece.py
@@ -502,6 +502,70 @@ async def rtag(event):
 #########################
 
 
+@client.on_message(
+        filters.command("stats") & filters.user(SUDO_USERS)
+    )
+    async def stats_func(_, message: Message):
+        if db is None:
+            return await message.reply_text(
+                "MONGO_DB_URI var not defined. Please define it first"
+            )
+        served_users = len(await mongo.get_served_users())
+        blocked = await mongo.get_banned_count()
+        text = f""" **ChatBot Stats:**
+        
+**Python Versiyon :** {pyver.split()[0]}
+**Pyrogram Versiyon :** {pyrover}
+**servis Kullanıcı:** {served_users} 
+**Engellenen kullanıcı:** {blocked}"""
+        await message.reply_text(text)
+
+    @app.on_message(
+        filters.command("broadcast") & filters.user(SUDO_USERS)
+    )
+    async def broadcast_func(_, message: Message):
+        if db is None:
+            return await message.reply_text(
+                "MONGO_DB_URI var not defined. Please define it first"
+            )
+        if message.reply_to_message:
+            x = message.reply_to_message.message_id
+            y = message.chat.id
+        else:
+            if len(message.command) < 2:
+                return await message.reply_text(
+                    "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
+                )
+            query = message.text.split(None, 1)[1]
+
+        susr = 0
+        served_users = []
+        susers = await mongo.get_served_users()
+        for user in susers:
+            served_users.append(int(user["user_id"]))
+        for i in served_users:
+            try:
+                await app.forward_messages(
+                    i, y, x
+                ) if message.reply_to_message else await app.send_message(
+                    i, text=query
+                )
+                susr += 1
+            except FloodWait as e:
+                flood_time = int(e.x)
+                if flood_time > 200:
+                    continue
+                await asyncio.sleep(flood_time)
+            except Exception:
+                pass
+        try:
+            await message.reply_text(
+                f"**Broadcasted Message to {susr} Users.**"
+            )
+        except:
+            pass
+
+
 print(">> Bot çalışmaktadir merak etme 🚀 @ByWolk bilgi alabilirsin <<")
 client.run_until_disconnected()
 run_until_disconnected()
